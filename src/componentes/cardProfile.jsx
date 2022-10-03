92c4a2c1c8ac52a8b36d719e7d38e6298bbbd3eb
Nickolinhas-rei-delas <cabocanick@gmail.com>
2022-10-02 22:39:41 -0300

    cards para impressão no encontrar

--- src/componentes/cardProfile.jsx
+++ src/componentes/cardProfile.jsx
@@ -1,8 +1,16 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-export default function cardProfile(){
+export default function cardProfile(props){
     return(
-    <Text>OIHJOIHOIH</Text>
-
+        <View><Text>nome</Text>
+            <Text>Localização</Text>
+            
+                
+            <View><Text>Jogo 1</Text> <Text>Jogo 2</Text></View>
+            <TouchableOpacity>Ver Perfil</TouchableOpacity>
+        
+        
+        </View>
+    
     )
 }
